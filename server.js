0572ffdfe01279cd8eac2f589a8f68e161bc17ac
samtalebi <samtalebi84@gmail.com>
2020-02-26 10:28:01 +0100

    import packages

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-
+// import body-parser, cors, express, morgan, errorhandler:
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+var cors = require('cors');
+const express = require('express');
+var morgan = require('morgan');
+var errorhandler = require('errorhandler');
 
 //create instance of express app:
 const app = express();
