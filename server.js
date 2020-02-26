4f95fa500034b21785607171a03bab31ab000925
samtalebi <samtalebi84@gmail.com>
2020-02-26 10:40:09 +0100

    fix bugs in server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 var cors = require('cors');
 const express = require('express');
-var morgan = require('morgan');
 var errorhandler = require('errorhandler');
+
+//import apiRouter
+const apiRouter = require('./api/api');
 
 //create instance of express app:
 const app = express();
