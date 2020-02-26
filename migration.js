5e34e4a75398808cde7d5919eb2408e02a48682f
samtalebi <samtalebi84@gmail.com>
2020-02-26 10:37:49 +0100

    create artist table

--- migration.js
+++ migration.js
@@ -4,3 +4,14 @@ const sqlite3 = require('sqlite3');
 //open database.sqlite file as sqlite3 database object
 const db = new sqlite3.Database('./database.sqlite');
 
+//Create Arist table if it doesn't exist
+db.serialize(()=>{
+    db.run(`DROP TABLE IF EXISTS Artist`);
+    db.run(`CREATE TABLE Artist(
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        date_of_birth TEXT NOT NULL,
+        biography TEXT NOT NULL,
+        is_currently_employed INTEGER DEFAULT 1
+    )`);
+})
