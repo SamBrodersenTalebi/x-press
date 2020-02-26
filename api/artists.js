6c75f5fe0bf33c87d66b059dca0fc73789c223e5
samtalebi <samtalebi84@gmail.com>
2020-02-26 11:04:57 +0100

    complete get handler in artists.js

--- api/artists.js
+++ api/artists.js
@@ -1,5 +1,27 @@
 //import express and create instance of express router
 const express = require('express');
 const artistsRouter = express.Router();
+//import sqlite3
+const sqlite3 = require('sqlite3');
+
+//check if process.env.TEST_DATABASE has been set, and if so load that database instead
+//it will be used for testing
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
+
+//add get handler 
+artistsRouter.get('/', (req, res, next)=>{
+    //db.all callback function takes two parameters 
+    db.all(`SELECT * FROM Artist WHERE Artist.is_currently_employed = 1`,
+    (err, rows)=>{
+        //check if there is an error
+        if(err){
+            //pass it to the next middleware function which is the errorhandler
+            next(err);
+        }else{
+            // res.json() Sends a JSON response composed of the specified data.
+            res.status(200).json({artists:rows});
+        }
+    });
+});
 
 module.exports = artistsRouter;
