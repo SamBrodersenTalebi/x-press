658aad4d0beb71630135bb2e54b069f78cfbdfe4
samtalebi <samtalebi84@gmail.com>
2020-02-26 11:46:35 +0100

    get handler for api/artist/:artistId

--- api/artists.js
+++ api/artists.js
@@ -8,6 +8,26 @@ const sqlite3 = require('sqlite3');
 //it will be used for testing
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
+//add router param
+artistsRouter.param('artistId', (req, res, next, artistId)=>{
+    db.get('SELECT * FROM Artist WHERE Artist.id=$artistId',
+    {
+        $artistId:artistId
+    },(error, artist)=>{
+        if(error){
+            next(error)
+        }else if(artits){
+            //attach to request object as artist
+            req.artist = artist;
+            //move on to next function 
+            next();
+        } else{
+            res.sendStatus(404);
+        }
+    })
+});
+
+
 //add get handler 
 artistsRouter.get('/', (req, res, next)=>{
     //db.all callback function takes two parameters 
@@ -24,23 +44,11 @@ artistsRouter.get('/', (req, res, next)=>{
     });
 });
 
-//add router param
-artistsRouter.param('artistId', (req, res, next, artistId)=>{
-    db.get('SELECT * FROM Artist WHERE id=$id',
-    {
-        $id:id
-    },(error, artist)=>{
-        if(error){
-            next(error)
-        }else if(artits){
-            //attach to request object as artist
-            req.artist = artist;
-            //move on to next function 
-            next();
-        } else{
-            res.sendStatus(404);
-        }
-    })
-});
+
+//add get handler for 'api/artist/:artistId
+artistsRouter.get(':artistId', (req, res, next)=>{
+    //the router param handles error handling and receives the artist at req.artist
+    res.status(200).json({artist: req.artist});
+})
 
 module.exports = artistsRouter;
