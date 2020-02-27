8e0e33687b280ed443d9d4205b964e5cea0655ca
samtalebi <samtalebi84@gmail.com>
2020-02-27 11:02:43 +0100

    set up series router

--- api/api.js
+++ api/api.js
@@ -3,10 +3,14 @@ const express = require('express')
 const apiRouter = express.Router()
 
 //import artistsRouter
-const artistsRouter = require('./artists');
+const artistsRouter = require('./artists.js');
+
+//import seriesRouter
+const seriesRouter = require('./series.js');
 
 //mount it at /artists
 apiRouter.use('/artists', artistsRouter);
+apiRouter.use('/series', seriesRouter)
 
 module.exports = apiRouter
 
---
+++ api/series.js
@@ -0,0 +1,8 @@
+//import express and create instance of express router
+const express = require('express');
+const seriesRouter = express.Router();
+
+
+
+//export router 
+module.exports = seriesRouter;
