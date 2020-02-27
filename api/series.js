2506a650dcf998403120223938208e3f9a2562ae
samtalebi <samtalebi84@gmail.com>
2020-02-27 11:19:45 +0100

    router handler for seriesId

--- api/series.js
+++ api/series.js
@@ -9,11 +9,27 @@ const sqlite3 = require('sqlite3');
 //it will be used for testing
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
+//Router param for handling the seriesId parameter
+seriesRouter.param('seriesId', (req, res, next, seriesId)=>{
+    const sql = "SELECT * FROM Series WHERE Series.id = $seriesId";
+    const values = {$seriesId: seriesId};
+    db.get(sql, values, (error, row)=>{
+        if(error){
+            next(error);
+        } else if(row){
+            //attach to request object as series
+            req.series = row;
+            next();
+        } else{
+            res.sendStatus(404);
+        }
+    })
+})
+
 
 //get handler at /
-
 seriesRouter.get('/', (req, res, next)=>{
-    const sql = "SELECT * FROM SERIES";
+    const sql = "SELECT * FROM Series";
     db.all(sql, (error, rows)=>{
         if(error){
             next(error);
