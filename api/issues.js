7bf4707735cd161108bd5977edc21692277d60d9
samtalebi <samtalebi84@gmail.com>
2020-02-27 13:04:54 +0100

    add post handler for issue

--- api/issues.js
+++ api/issues.js
@@ -22,6 +22,48 @@ issuesRouter.get('/', (req, res, next)=> {
             res.send(200).json({issues:rows});
         }
     })
-})
+});
+
+//post handler at /
+issuesRouter.post('/', (req, res, next)=>{
+    //req.body contains paramters sent up from the user in a POST request
+    const name = req.body.issue.name;
+    const issueNumber =req.body.issue.issueNumber;
+    const publicationDate =req.body.issue.publicationDate;
+    const artistId =req.body.issue.artistId;
+    const artistSql = "SELECT * FROM Artist WHERE Artist.id = $artistId";
+    const artistValue = {$artistId: artistId};
+    
+    //need to make sure that the artistid exists
+    db.get(artistSql, artistValue, (error, row)=>{
+        if(error){
+            next(error);
+        } else{
+            if(!name || !issueNumber || !publicationDate){
+                return res.send(400);
+            }
+            const sql = "INSERT INTO Issue(name, issue_number, publication_date, artist_id, series_id) VALUES($name, $issueNumber, $publicationDate, $artistId, $seriesId)";
+            const values = {
+                $name: name,
+                $issueNumber: issueNumber,
+                $publicationDate: publicationDate,
+                $artistId: row,
+                $seriesId: req.params.seriesId
+            };
+            //run query
+            db.run(sql, values, function(error){
+                if(error){
+                    next(error);
+                }else{
+                    //select created row with lastID
+                    db.get(`SELECT * FROM Issue WHERE Issue.id = ${this.lastID}`, (error, row)=>{
+                        res.status(201).json({issue:row})
+                    })
+                }
+            })
+        }
+    })
+    
+});
 
 module.exports = issuesRouter;
