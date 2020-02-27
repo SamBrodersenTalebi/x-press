ca6b49ab9454459e8599f781da066a1f538e8e69
samtalebi <samtalebi84@gmail.com>
2020-02-27 12:24:15 +0100

    import and mount issueRouter in series.js

--- api/series.js
+++ api/series.js
@@ -1,6 +1,7 @@
 //import express and create instance of express router
 const express = require('express');
 const seriesRouter = express.Router();
+const issuesRouter = require('./issues.js')
 
 //import sqlite3
 const sqlite3 = require('sqlite3');
@@ -8,6 +9,9 @@ const sqlite3 = require('sqlite3');
 //check if process.env.TEST_DATABASE has been set, and if so load that database instead
 //it will be used for testing
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
+
+//Mount issuesRouter at /:seriesId/issues
+seriesRouter.use('/:seriesId/issues', issuesRouter);
 
 //Router param for handling the seriesId parameter
 seriesRouter.param('seriesId', (req, res, next, seriesId)=>{
